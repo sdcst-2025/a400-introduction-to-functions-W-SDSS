a72fdcc817e78b52a4ac65a93c5107ffccdfd8bb
W-SDSS <wakanai027@deltalearns.ca>
2026-01-27 09:37:13 -0800

    w

--- title.py
+++ title.py
@@ -1,4 +1,5 @@
 #instruction
 
-print("This is number guessing game.")
-print("You will guess number from 0 to 100.")
+print("This is a number guessing game.")
+print("You will guess number from 0 to 10.")
+print("Number of times you have guessed is also show up.")
