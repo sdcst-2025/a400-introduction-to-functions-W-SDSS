ac561cc98fb44d722ee388f95ef8a61c90370a7e
W-SDSS <wakanai027@deltalearns.ca>
2026-01-28 06:46:29 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -10,6 +10,22 @@ plays the game
 This will be silimar to something you have already done, but in this task you 
 are breaking the code up into different sections to make each a function.
 """
+import random
 
-import title
-import game
+def title():
+  print("---Number Guessing Game!!---")
+  print("I am thinking of a number 1 to 10.")[
+  print("Try to guess it")
+
+def game():
+  num = random.randint(1, 11)
+  guess = 0
+  while guess != num:
+    guess = int(input("Enter your guess: "))
+    if guess < num:
+      print("Its too low")
+    elif guess > num:
+      print("its too high")
+    else:
+      print("correct!")
+      
